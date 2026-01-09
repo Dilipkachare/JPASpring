bf6d47950d9225732f1e6e7c761c3272639b1bd8
Dilipkachare <dilipkachare888@gmail.com>
2026-01-09 18:18:07 +0530

    revise

--- 04-firstSpringBoot/src/main/java/Dilip/com/firstSpring/SendMailAppStart.java
+++ 04-firstSpringBoot/src/main/java/Dilip/com/firstSpring/SendMailAppStart.java
@@ -1,12 +1,9 @@
 package Dilip.com.firstSpring;
 
 import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.stereotype.Component;
 
-import Dilip.com.Spring.enity.AccountsPk;
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Table;
 
 @Component
 public class SendMailAppStart implements CommandLineRunner{
@@ -14,18 +11,6 @@ public class SendMailAppStart implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception{
 		System.out.println("Application is started to send the mail....");
-
-		@Table(name = "BANK_ACCOUNTS")
-		public class Account {
-
-			@Column(name = "BRANCH_NAME")
-			private String branchName;
-
-			@Column(name = "MIN_BAL")
-			private Double minBal;
-
-			@EmbeddedId
-			private AccountsPk accPk;
 
 	}
 	
